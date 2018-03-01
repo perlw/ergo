d5ebb0e0fcafc3c15116b42a8e086665b04170e2
Per Lönn Wege <per@perlw.se>
2018-03-01 22:02:14 +0100

    Toy v2

--- main.go
+++ main.go
@@ -5,9 +5,7 @@ import (
 	"net"
 )
 
-func main() {
-	fmt.Println("Launching... well.. me")
-
+func telnet() {
 	ln, err := net.Listen("tcp", ":23")
 	if err != nil {
 		panic(err)
@@ -20,4 +18,28 @@ func main() {
 			conn.Close()
 		}()
 	}
+}
+
+func web() {
+	ln, err := net.Listen("tcp", ":80")
+	if err != nil {
+		panic(err)
+	}
+
+	for {
+		conn, _ := ln.Accept()
+		go func() {
+			conn.Write([]byte("Ping\n"))
+			conn.Close()
+		}()
+	}
+}
+
+func main() {
+	fmt.Println("Launching... well.. me")
+
+	var forever chan int = nil
+	go telnet()
+	go web()
+	<-forever
 }
