1e4fbb2e33d5a903fd40b469618fec15a2428553
Per Lönn Wege <per@perlw.se>
2018-03-03 14:53:58 +0100

    Initial game workings

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"bufio"
 	"bytes"
-	"fmt"
+	"errors"
 	"html/template"
 	"log"
 	"net"
@@ -11,12 +11,73 @@ import (
 	"strings"
 )
 
+var (
+	// ErrUnrecognizedCommand is returned when an unrecognized command is input to the interpreter
+	ErrUnrecognizedCommand = errors.New("unrecognized command")
+)
+
+var gamePrompt = []byte("\r\n> ")
+
+type GameCommand struct {
+	Cmd     string
+	Aliases []string
+	Action  func(arguments []string) ([]byte, error)
+}
+
+type GameState struct {
+	Commands []GameCommand
+}
+
+func NewGameState() *GameState {
+	state := &GameState{
+		Commands: []GameCommand{
+			{
+				Cmd:     "help",
+				Aliases: []string{"h", "?"},
+				Action: func(arguments []string) ([]byte, error) {
+					return []byte("TBD"), nil
+				},
+			},
+		},
+	}
+	return state
+}
+
+func (s *GameState) InterpretCommand(action string, arguments []string) ([]byte, error) {
+	for _, cmd := range s.Commands {
+		matched := false
+		if cmd.Cmd == action {
+			matched = true
+		} else {
+			for _, alias := range cmd.Aliases {
+				if alias == action {
+					matched = true
+					break
+				}
+			}
+		}
+
+		if matched {
+			return cmd.Action(arguments)
+		}
+	}
+
+	return nil, ErrUnrecognizedCommand
+}
+
 func spawnGame(conn net.Conn) {
 	defer conn.Close()
 
-	conn.Write([]byte(">"))
+	state := NewGameState()
 
+	w := bufio.NewWriter(conn)
 	r := bufio.NewReader(conn)
+
+	w.Write([]byte("The Dungeon\r\n==========="))
+	w.Write(gamePrompt)
+	w.Flush()
+
+	lineEnd := false
 	buffer := make([]byte, 1)
 	line := bytes.Buffer{}
 	for {
@@ -28,6 +89,7 @@ func spawnGame(conn net.Conn) {
 			continue
 		}
 
+		// Telnet commands
 		if buffer[0] == 0xff {
 			cmd := make([]byte, 2)
 			n, err := r.Read(cmd)
@@ -35,24 +97,53 @@ func spawnGame(conn net.Conn) {
 				log.Println("could not read cmd from client, ", err)
 				return
 			}
-			fmt.Println("cmd: ", cmd)
-		} else if buffer[0] == '\n' {
-			fmt.Println("client says, ", line.Bytes(), line.String())
+			log.Println("cmd: ", cmd)
+		} else if buffer[0] == '\n' || buffer[0] == '\r' {
+			if line.Len() == 0 {
+				if !lineEnd {
+					lineEnd = true
+					continue
+				}
+			}
+			lineEnd = false
 
-			if strings.TrimRight(line.String(), "\r") == "quit" {
-				conn.Write([]byte("BYE"))
-				return
+			msg := strings.TrimRight(line.String(), "\r\n")
+			// Interpret the command
+			if msg == "quit" {
+				break
+			} else {
+				words := strings.Split(msg, " ")
+				action := strings.ToLower(words[0])
+				arguments := words[1:]
+
+				output, err := state.InterpretCommand(action, arguments)
+				if err != nil {
+					log.Println("could not interpret", err)
+
+					if err == ErrUnrecognizedCommand {
+						w.Write([]byte("Sorry, I don't know how to '" + action + "'..."))
+					}
+				} else {
+					if output != nil {
+						w.Write(output)
+					}
+				}
+				w.Write([]byte("\r\n"))
 			}
 
 			line.Reset()
-			conn.Write([]byte(">"))
+			w.Write(gamePrompt)
+			w.Flush()
 		} else {
 			line.WriteByte(buffer[0])
 		}
 	}
+
+	w.Write([]byte("BYE"))
+	w.Flush()
 }
 
-func telnet() {
+func serveGame() {
 	ln, err := net.Listen("tcp", ":23")
 	if err != nil {
 		panic(err)
@@ -64,7 +155,7 @@ func telnet() {
 	}
 }
 
-func web() {
+func serveWeb() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		t, err := template.ParseFiles("index.html")
 		if err != nil {
@@ -81,10 +172,10 @@ func web() {
 }
 
 func main() {
-	fmt.Println("Launching... well.. me")
+	log.Println("Launching... well.. me")
 
-	go telnet()
-	go web()
+	go serveGame()
+	go serveWeb()
 
 	var forever chan int = nil
 	<-forever
