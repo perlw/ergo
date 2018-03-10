0c7e2335f2ede5cdfc7e17b512793f0fd13a9d97
Per Lönn Wege <per@perlw.se>
2018-03-10 16:34:41 +0100

    Toying

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"log"
 	"net"
 	"net/http"
+	"strings"
+	"sync"
 )
 
 func serveGame() {
@@ -20,15 +22,121 @@ func serveGame() {
 	}
 }
 
-func serveWeb() {
-	apiMux := http.NewServeMux()
-	apiMux.HandleFunc("/note", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("foo"))
-	})
+type Router struct {
+	mu sync.RWMutex
+	m  map[string]routerEntry
+}
 
-	mux := http.NewServeMux()
-	mux.Handle("/api/", apiMux)
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+type routerEntry struct {
+	h       http.Handler
+	pattern string
+}
+
+func NewRouter() *Router {
+	return &Router{}
+}
+
+func (mux *Router) Handler(r *http.Request) http.Handler {
+	mux.mu.RLock()
+	defer mux.mu.RUnlock()
+
+	v, ok := mux.m[r.URL.Path]
+	if ok {
+		return v.h
+	}
+
+	fmt.Println("here I am", r.URL.Path)
+	path := strings.Split(r.URL.Path, "/")
+	if len(path) > 1 {
+		path = path[1:]
+	}
+	fmt.Println(path)
+
+	longest := -1
+	for _, entry := range mux.m {
+		entryPath := strings.Split(entry.pattern, "/")
+		if len(entryPath) > 1 {
+			entryPath = entryPath[1:]
+		}
+		fmt.Println("entry", entryPath)
+
+		count := 0
+		for t := range entryPath {
+			if path[t] != entryPath[t] {
+				break
+			} else {
+				count++
+			}
+		}
+		if count > 0 && count > longest {
+			fmt.Println("marking", longest)
+			longest = count
+		}
+		fmt.Println("longest", path, "vs", entryPath, longest)
+	}
+
+	if longest > -1 {
+		subPath := "/" + strings.Join(path[:longest], "/")
+		fmt.Println("going with", subPath)
+		v, ok = mux.m[subPath]
+		if ok {
+			return v.h
+		}
+	}
+
+	return http.NotFoundHandler()
+}
+
+func (mux *Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.RequestURI == "*" {
+		if r.ProtoAtLeast(1, 1) {
+			w.Header().Set("Connection", "close")
+		}
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	h := mux.Handler(r)
+
+	h.ServeHTTP(w, r)
+}
+
+func (mux *Router) Handle(pattern string, handler http.Handler) {
+	mux.mu.Lock()
+	defer mux.mu.Unlock()
+
+	if pattern == "" {
+		panic("http: invalid pattern")
+	}
+	if handler == nil {
+		panic("http: nil handler")
+	}
+	if _, exist := mux.m[pattern]; exist {
+		panic("http: multiple registrations for " + pattern)
+	}
+
+	if mux.m == nil {
+		mux.m = make(map[string]routerEntry)
+	}
+	mux.m[pattern] = routerEntry{h: handler, pattern: pattern}
+}
+
+func serveWeb() {
+	router := NewRouter()
+
+	router2 := NewRouter()
+	router2.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("inside api"))
+	}))
+	router2.Handle("/note", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("note me senpai"))
+	}))
+
+	router.Handle("/foo", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("foo"))
+	}))
+	router.Handle("/api", router2)
+	router.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		t, err := template.ParseFiles("index.html")
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -36,9 +144,9 @@ func serveWeb() {
 		}
 
 		t.Execute(w, nil)
-	})
+	}))
 
-	err := http.ListenAndServe(":80", mux)
+	err := http.ListenAndServe(":80", router)
 	if err != nil {
 		log.Fatal("could not start server,", err)
 	}
